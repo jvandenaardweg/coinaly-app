408139d23d69b8c36bfecac53ccf58730d925f39
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-04-20 16:41:29 +0200

    Add horizontal scroller to balance page

---
+++ src/components/HorizontalScroller.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="horizontal-scroller">
+    <div class="horizontal-scroller-container">
+      <div class="horizontal-scroller-item">
+        <div class="card">
+          <div class="card-body">High performing trade</div>
+        </div>
+      </div>
+      <div class="horizontal-scroller-item">
+        <div class="card">
+          <div class="card-body">High performing trade</div>
+        </div>
+      </div>
+      <div class="horizontal-scroller-item">
+        <div class="card">
+          <div class="card-body">High performing trade</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HorizontalScroller'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/bootstrap/setting";
+@import "~bootstrap/scss/mixins/breakpoints";
+
+.horizontal-scroller {
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  margin-left: -0.75rem;
+  margin-right:-0.75rem;
+
+  @include media-breakpoint-up(md) {
+    white-space: inherit;
+    margin-left: inherit;
+    margin-right: inherit;
+  }
+
+  .horizontal-scroller-container {
+    overflow-x: auto;
+  }
+
+  .horizontal-scroller-item {
+    display: inline-block;
+
+    @include media-breakpoint-up(md) {
+      display: block;
+      margin-bottom: 0.5rem;
+    }
+
+  }
+}
+
+</style>
--- src/pages/balances/Index.vue
+++ src/pages/balances/Index.vue
@@ -1,38 +1,25 @@
 <template>
   <div id="balances">
-    <div class="row justify-content-center">
-      <div class="col">
-        <div class="card card-100-xs bg-blue" style="margin-top:-1.30rem;">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card card-100-xs bg-blue h-100" style="margin-top:-1.30rem;">
           <div class="card-body text-center">
             <h1 class="h1 text-white mb-1 font-weight-bold">&euro; 12.035,51</h1>
             <h2 class="h5 text-light font-weight-normal">2.41231 BTC</h2>
           </div>
         </div>
       </div>
-    </div>
-
-    <div style="overflow-x: auto; overflow-y: hidden; white-space: nowrap;margin-left: -0.75rem;margin-right:-0.75rem;">
-      <div class="p-3" style="overflow-x: auto">
-        <div class="d-inline-block">
-          <div class="card">
-            <div class="card-body">High performing trade</div>
-          </div>
-        </div>
-        <div class="d-inline-block">
-          <div class="card">
-            <div class="card-body">High performing trade</div>
-          </div>
-        </div>
-        <div class="d-inline-block">
-          <div class="card">
-            <div class="card-body">High performing trade</div>
-          </div>
-        </div>
+      <div class="col-md-6">
+        <HorizontalScroller></HorizontalScroller>
       </div>
     </div>
 
+
+
     <div class="row justify-content-center">
       <div class="col">
+
+         <line-chart :data="chartData" :options="{responsive: true, maintainAspectRatio: true}"></line-chart>
 
 
         <div class="card mt-4">
@@ -54,7 +41,29 @@
 </template>
 
 <script>
+import LineChart from '@/components/charts/LineChart.js'
+import HorizontalScroller from '@/components/HorizontalScroller'
+
 export default {
-  name: 'PageBalances'
+  name: 'PageBalances',
+  components: {
+    HorizontalScroller,
+    LineChart
+  },
+  computed: {
+    chartData () {
+      return {
+        data: [
+          {
+            x: 10,
+            y: 20
+          }, {
+            x: 15,
+            y: 10
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
