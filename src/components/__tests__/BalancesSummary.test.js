976b380b0e05243fd975eb1fb950b85a7d8e8dc4
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-05-17 14:25:26 +0200

    Fix failing test

--- src/components/__tests__/BalancesSummary.test.js
+++ src/components/__tests__/BalancesSummary.test.js
@@ -1,14 +1,42 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import BalancesSummary from '@/components/BalancesSummary.vue'
+
+import balancesModule from '@/store/modules/balances'
+import pricesModule from '@/store/modules/prices'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
 
 describe('components/BalancesSummary.vue', () => {
   let component
+  let store
 
   beforeEach(() => {
-    component = shallowMount(BalancesSummary)
+    store = new Vuex.Store({
+      modules: {
+        balances: balancesModule,
+        prices: pricesModule
+      }
+    })
+
+    component = shallowMount(BalancesSummary, {
+      store,
+      localVue,
+      stubs: ['router-link', 'router-view']
+    })
   })
 
   it('renders a vue instance', () => {
     expect(component.isVueInstance()).toBe(true)
+  })
+
+  it('renders a loading text when loading the needed data', () => {
+    expect(component.find('h1').text()).toBe('Loading...')
+  })
+
+  it('renders an empty element for the additional pricing', () => {
+    expect(component.find('h2').text()).toBe('')
   })
 })
