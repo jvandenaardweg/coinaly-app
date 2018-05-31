29b4288551225c916528722790e3f97044c33bf0
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2018-05-31 14:19:15 +0200

    Auto sync every X minutes

--- src/components/btn/Sync.vue
+++ src/components/btn/Sync.vue
@@ -9,17 +9,42 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'BtnSync',
   data: () => ({
     isLoading: false,
     isDisabled: false,
-    timeout: 10000
+    timeout: 10000,
+    lastSynced: new Date(),
+    syncInterval: 5 * 60 * 1000 // 5 minutes
   }),
   computed: {
+    ...mapGetters({
+      selectedExchange: 'exchanges/selected'
+    }),
     timeoutSeconds () {
       return (this.timeout / 1000)
+    },
+    lastSyncDifference () {
+      return this.lastSynced.getTime() - new Date().getTime()
     }
+  },
+  mounted () {
+    // Sync user data every X minutes automatically when the user has the website open
+    setInterval(() => {
+      // Check if the user has not already synced himself
+      // If not, we auto sync
+      const currentTimestamp = new Date().getTime()
+      const lastSyncedTimestamp = this.lastSynced.getTime()
+      const shouldSync = (currentTimestamp - lastSyncedTimestamp) > this.syncInterval
+
+      if (shouldSync) {
+        this.isDisabled = false // So the auto-sync does not show an alert to the user
+        this.handleClick()
+      }
+    }, this.syncInterval)
   },
   methods: {
     async handleClick () {
@@ -28,6 +53,7 @@ export default {
         try {
           await this.forceRefreshUserData()
           this.activateTimeout()
+          this.lastSynced = new Date()
         } finally {
           this.isDisabled = true
           this.isLoading = false
@@ -46,11 +72,17 @@ export default {
       window.alert(`Please wait ${this.timeoutSeconds} seconds before syncing your balance and orders again.`)
     },
     forceRefreshUserData () {
-      return Promise.all([
-        this.$store.dispatch('balances/getAll', {forceRefresh: true}),
-        this.$store.dispatch('orders/getAllOpenOrders', {forceRefresh: true}),
-        this.$store.dispatch('orders/getAllClosedOrders', {forceRefresh: true})
-      ])
+      if (this.selectedExchange === 'binance') {
+        return Promise.all([
+          this.$store.dispatch('balances/getAll', {forceRefresh: true})
+        ])
+      } else {
+        return Promise.all([
+          this.$store.dispatch('balances/getAll', {forceRefresh: true}),
+          this.$store.dispatch('orders/getAllOpenOrders', {forceRefresh: true}),
+          this.$store.dispatch('orders/getAllClosedOrders', {forceRefresh: true})
+        ])
+      }
     }
   }
 }
